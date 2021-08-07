32d258722f70b9c4001142500b071381b85eb499
Alf-André Walla <fwsgonzo@hotmail.com>
2021-08-07 09:44:21 +0200

    Update mandelbrot example

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 file="$1"
 tenant="$2"
-host="http://141.0.231.216:8080"
+host="http://127.0.0.1:8080"
 key="12daf155b8508edc4a4b8002264d7494"
 
 source scripts/live_update.sh "$host" "$tenant" "$key" "$file"
