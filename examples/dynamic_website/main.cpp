f2c3bc8e73f08fd254573f562a55074565c4c9a1
Alf-André Walla <fwsgonzo@hotmail.com>
2021-08-13 13:09:39 +0200

    Stateful C++ example now works

--- examples/dynamic_website/main.cpp
+++ examples/dynamic_website/main.cpp
@@ -1,15 +1,15 @@
 #include "varnish_api.hpp"
 
 #include <cstdio>
-
 extern "C" void retrieve_json(void*, size_t, size_t);
 extern "C" void set_json(void*, size_t, size_t);
+EMBED_BINARY(index_html, "../index.html")
 
 int main() {
 	printf("C++ JSON example main\n");
 }
-
-EMBED_BINARY(index_html, "../index.html")
+/* Make pre-allocated 4KB result */
+static std::vector<uint8_t> result(4096);
 
 extern "C" __attribute__((used))
 void my_backend(const char *arg)
@@ -17,37 +17,33 @@ void my_backend(const char *arg)
 	const std::string path {arg};
 	if (path == "/" || path == "/w") {
 		Backend::response("text/html", index_html, index_html_size);
-	} else if (path == "/get") {
-		/* Make room for a 1KB result */
-		std::vector<uint8_t> result(1024);
+	} else if (path == "/w/get") {
 		/* Call 'retrieve_json' in storage and retrieve result */
 		long len = Storage::call(retrieve_json, result);
 		result.resize(len);
-		/* Ship the result on the wire, as JSON */
-		Backend::response("application/json", result);
-	} else {
-		Backend::response("text/plain", "Unknown location");
+		/* Ship the result on the wire */
+		Backend::response("text/plain", result);
 	}
+	/* TODO: 404 mechanism here */
+	Backend::response("text/plain", "Unknown location");
 }
 
 extern "C" __attribute__((used))
 void my_post_backend(const char* /* arg */, void* data, size_t len)
 {
-	std::vector<uint8_t> result(1024);
-
 	size_t reslen = Storage::call(set_json, data, len, result);
 	result.resize(reslen);
 
-	Backend::response("application/json", result);
+	Backend::response("text/plain", result);
 }
 
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
-static json g_json;
+std::string text;
 
 void retrieve_json(void*, size_t, size_t /* reslen */)
 {
-	Storage::response(g_json.dump(4));
+	Storage::response(text);
 }
 
 void set_json(void* data, size_t len, size_t /* reslen */)
@@ -55,7 +51,8 @@ void set_json(void* data, size_t len, size_t /* reslen */)
 	const uint8_t* data_begin = (uint8_t*)data;
 	const uint8_t* data_end   = data_begin + len;
 
-	g_json = json::parse(data_begin, data_end);
+	json j = json::parse(data_begin, data_end);
+	text += j["text"].get<std::string>() + "\n";
 
-	Storage::response(g_json.dump(4));
+	Storage::response(text);
 }
