0fefb9c5da053d409e9eb100cab6d78c6f9fdd84
Alf-André Walla <fwsgonzo@hotmail.com>
2021-10-02 14:23:16 +0200

    Add benchmark automation script for mandelbrot variants

---
+++ examples/mandelbrot/automate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+file="${1-build/mandelbrot_avx2}"
+tenant="zpizza.com"
+host="http://127.0.0.1:8080"
+key="12daf155b8508edc4a4b8002264d7494"
+
+echo "Sending $file to tenant $tenant at $host"
+curl -H "X-PostKey: $key" -H "Host: $tenant" --data-binary "@$file" -X POST $host
+
+N=20
+WRK="$HOME/github/wrk/wrk"
+
+for T in $(seq 1 $N);
+do
+	output=$($WRK -c ${T} -t ${T} -d 5s $host/z | grep Latency)
+	arr=($output)
+	average=${arr[1]}
+	lowest=${arr[2]}
+	highest=${arr[3]}
+	echo $T $average $lowest $highest
+done
